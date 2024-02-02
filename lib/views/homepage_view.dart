bad656aa7a7ef7465692b63a1fa32d341084b30b
Manoel <manoelpiovesan@gmail.com>
2024-02-02 12:28:38 -0300

    Adding list.

--- lib/views/homepage_view.dart
+++ lib/views/homepage_view.dart
@@ -9,14 +9,45 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  int page = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('CBO List'),
+        actions: [
+          Row(
+            children: [
+              Text('Página $page'),
+              const SizedBox(width: 10),
+            ],
+          ),
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  if(page > 0) page--;
+                });
+              },
+              icon: const Icon(Icons.arrow_back)),
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  page++;
+                });
+              },
+              icon: const Icon(Icons.arrow_forward)),
+        ],
+        title: const Row(
+          mainAxisSize: MainAxisSize.min,
+          children:  [
+            Icon(Icons.work),
+            SizedBox(width: 10),
+            Text('CBOs'),
+          ],
+        ),
       ),
-      body: const Center(
-        child: CboList(),
+      body: Center(
+        child: CboList(page: page),
       ),
     );
   }
--- lib/views/list/cbo_list.dart
+++ lib/views/list/cbo_list.dart
@@ -2,7 +2,9 @@ import 'package:cbo_front/consumer/cbo_consumer.dart';
 import 'package:flutter/material.dart';
 
 class CboList extends StatefulWidget {
-  const CboList({super.key});
+  final int page;
+
+  const CboList({required this.page, super.key});
 
   @override
   State<CboList> createState() => _CboListState();
@@ -13,24 +15,27 @@ class _CboListState extends State<CboList> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(future: cboConsumer.getAll(), builder: (context, snapshot) {
-      if (snapshot.connectionState == ConnectionState.done) {
-        if (snapshot.hasError) {
-          return Center(child: Text('Error: ${snapshot.error}'));
-        }
+    return FutureBuilder(
+        future: cboConsumer.getAll(page: widget.page),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            }
 
-        return ListView.builder(
-          itemCount: snapshot.data!.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(snapshot.data![index].code),
-              subtitle: Text(snapshot.data![index].description),
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(snapshot.data![index].code),
+                  subtitle: Text(snapshot.data![index].description),
+                  leading: const Icon(Icons.work),
+                );
+              },
             );
-          },
-        );
-      }
+          }
 
-      return const Center(child: CircularProgressIndicator());
-    });
+          return const Center(child: CircularProgressIndicator());
+        });
   }
 }
